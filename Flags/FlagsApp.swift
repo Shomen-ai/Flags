fc61956e0288811f2f47233f1c8b62c732942438
dmitryShaimanov <jthehuman37@yandex.ru>
2024-03-26 18:44:23 +0300

    Initial Commit

---
+++ Flags/FlagsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagsApp.swift
+//  Flags
+//
+//  Created by Дмитрий Шайманов on 26.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct FlagsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
